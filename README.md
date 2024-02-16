34a1cc65fd5d874162c5eff5689479d8d9221ef7
Jamie Land <hokie10@gmail.com>
2024-02-16 15:05:44 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Gradle Demo
+
+Created a basic demo here to show how we can setup a management port inorder to handel our liveness probes even if the network is totally jammed up with traffic.
+
+Run with:
+
+```
+./gradlew bootRun
+```
---
+++ src/main/java/com/example/restservice/GreedyController.java
@@ -0,0 +1,25 @@
+package com.example.restservice;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.websocket.server.PathParam;
+
+@RestController
+public class GreedyController {
+
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+	@GetMapping("/sleep/{time}")
+	public String sleep(@PathVariable(value = "time") Integer time) throws InterruptedException {
+		System.out.println("Sleeping for 10 seconds...");
+		Thread.sleep(time * 1000);
+		return "Who dares to wake me up? (slept for " + time + " seconds)";
+
+	}
+}
