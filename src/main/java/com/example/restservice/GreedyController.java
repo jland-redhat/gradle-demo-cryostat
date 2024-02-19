1d2be9cf567fc6080860ea267dcc0c9a8e322686
Jamie Land <hokie10@gmail.com>
2024-02-19 15:50:46 -0500

    Adding endpoint to consume memory

--- src/main/java/com/example/restservice/GreedyController.java
+++ src/main/java/com/example/restservice/GreedyController.java
@@ -22,4 +22,27 @@ public class GreedyController {
 		return "Who dares to wake me up? (slept for " + time + " seconds)";
 
 	}
+
+
+	@GetMapping("/memory/consume")
+	public String consumeMemory() { 
+			try {
+			// Define the size of the array to consume memory
+			long arraySize = 100; // Adjust the size as needed
+
+			// Allocate memory by creating a large array
+			byte[] byteArray = new byte[(int) arraySize];
+			System.out.println("Allocated " + (arraySize / (1024 * 1024)) + " MB of memory.");
+			
+			// Optionally, populate the array to ensure memory is actually used
+			for (int i = 0; i < byteArray.length; i++) {
+					byteArray[i] = (byte) i;
+			}
+			sleep(3);
+			return "Freeing (consumed " + (arraySize / (1024 * 1024)) + " MB of memory)";
+		} catch (Exception e) {
+				e.printStackTrace();
+		}
+		return null;
+	}
 }
